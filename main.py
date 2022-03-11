0fffbb94a7c76de8cba27a162af024397e96e286
nurkhanlaiyk <86560310+nurkhanlaiyk@users.noreply.github.com>
2022-03-11 21:24:27 +0600

    Update main.py
    I guess from this file it is not that clear. I will also send the .pynb notebook by emai.

--- main.py
+++ main.py
@@ -81,3 +81,318 @@ class UNET(nn.Module):
             x = self.ups[idx+1](concat_skip)
 
         return self.final_conv(x)
+
+    
+    
+    #DATASET 
+    
+class BTSDataset(Dataset): #FOR TRAIN AND VALIDATION
+    def __init__(self, image_dir, mask_dir=None, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path =self.mask_dir+'/'+self.images[index].replace('.jpg','.png') #os.path.join(self.mask_dir, self.images[index])#.replace(".png", ".jpg"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        mask[mask == 255.0] = 1.0
+        image = np.array(Image.open(img_path).convert("RGB"))
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
+    
+    
+#DATASET FOR TEST
+class BTSDataset_Test(Dataset):
+    def __init__(self, image_dir,transform=None):
+        self.image_dir=image_dir
+        self.transform=transform
+        self.images=os.listdir(image_dir)
+        
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self,index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        image = np.array(Image.open(img_path).convert("RGB"))
+        if self.transform is not None:
+            augmentations=self.transform(image=image)
+            image=augmentations["image"]
+        return image
+ 
+
+
+os.makedirs('/kaggle/working/saved_images')
+os.makedirs('/kaggle/working/saved_test/')
+
+
+
+
+
+#UTILS
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint, model):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+def get_loaders(
+    train_dir,
+    train_maskdir,
+    val_dir,
+    val_maskdir,
+    test_dir,
+    batch_size,
+    train_transform,
+    val_transform,
+    test_transform,
+    num_workers=4,
+    pin_memory=True,
+):
+    train_ds = BTSDataset(
+        image_dir=train_dir,
+        mask_dir=train_maskdir,
+        transform=train_transform,
+    )
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+    )
+
+    val_ds = BTSDataset(
+        image_dir=val_dir,
+        mask_dir=val_maskdir,
+        transform=val_transform,
+    )
+
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=False,
+    )
+    test_ds= BTSDataset_Test(###################################################
+        image_dir=test_dir,
+        #mask_dir=test_maskdir,
+        transform=test_transform,
+    )
+    test_loader = DataLoader(############################3
+        test_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=False,
+    )
+    
+    #print(val_loader)
+    return train_loader, val_loader, test_loader
+
+def check_accuracy(loader, model, device="cuda"):
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device)
+            y = y.to(device).unsqueeze(1)
+            preds = torch.sigmoid(model(x))
+            
+            preds = (preds > 0.5).float()
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += ( (preds * y).sum()) / (#2*
+                (preds + y).sum() + 1e-8
+            )
+
+    print(
+        f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+    )
+    print(f"Dice score: {dice_score/len(loader)}")
+    model.train()
+
+def save_predictions_as_imgs(
+    loader, model, folder="/kaggle/working/saved_images/", device="cuda"
+):
+    model.eval()
+    for idx, (x, y) in enumerate(loader):
+        print(idx)
+        x = x.to(device=device)
+        with torch.no_grad():
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+        torchvision.utils.save_image(
+            preds, f"{folder}/pred_{idx}.png"
+        )
+        torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
+
+    model.train()
+
+    
+def save_test(loader, model, folder='/kaggle/working/saved_test/',device='cuda'):
+    test_list=[i for i in os.listdir('../input/test-bts/imgs')]
+
+    model.eval()
+    for idx, x in enumerate(loader):
+        #print(idx)
+        x = x.to(device=device)
+        
+        with torch.no_grad(): 
+            for j,i in zip(range(16),x):
+                
+            
+                preds = torch.sigmoid(model(i.unsqueeze(0)))
+                preds = (preds > 0.5).float()
+                torchvision.utils.save_image(
+                    preds, test_list[j])
+               
+            test_list=test_list[16:]
+       
+    model.train()
+
+    
+    
+    
+    LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 80
+NUM_WORKERS = 2
+IMAGE_HEIGHT =200  # 1280 originally
+IMAGE_WIDTH = 280# 1918 originally
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "/kaggle/input/segmentation-bts/train/imgs"
+TRAIN_MASK_DIR = "/kaggle/input/segmentation-bts/train/masks"
+VAL_IMG_DIR = "/kaggle/input/segmentation-bts/val/imgs"
+VAL_MASK_DIR = "/kaggle/input/segmentation-bts/val/masks"
+TEST_IMG_DIR="/kaggle/input/test-bts/imgs"
+
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        # forward
+        with torch.cuda.amp.autocast():
+            predictions = model(data)#torch.sigmoid(model(data)) CORRECT
+            #print('predictions: ', predictions)
+            #plt.imshow(predictions.detach().cpu().numpy()[0][0], cmap='gray')
+            #print('target: ', targets)
+            loss = loss_fn(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+    test_transforms= A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+    
+    
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader,test_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        TEST_IMG_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transforms,
+        test_transforms,
+        NUM_WORKERS,
+        PIN_MEMORY,
+    )
+
+    if LOAD_MODEL:
+        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+
+
+    check_accuracy(val_loader, model, device=DEVICE)
+    scaler = torch.cuda.amp.GradScaler()
+    #test_list=[i for i in os.listdir('../input/test-bts/imgs')]
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        
+        # save model
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer":optimizer.state_dict(),
+        }
+        save_checkpoint(checkpoint)
+
+
+        check_accuracy(val_loader, model, device=DEVICE)
+        save_test(test_loader, model, folder='/kaggle/working', device=DEVICE)
+
+
+if __name__ == "__main__":
+    main()
